6a2bbdb92f4c12f230234cccb0045481ac42fdff
Lucio Ariel <lucioariel.furtado@gmail.com>
2022-07-18 12:09:55 -0300

    fix workflows

--- next.config.js
+++ next.config.js
@@ -2,8 +2,8 @@
 const nextConfig = {
   reactStrictMode: true,
   presets: ["next/babel"],
-  basePath: "/teste",
-  assetPrefix: "/teste",
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH,
+  assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH,
 };
 
 module.exports = nextConfig;
